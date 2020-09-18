0b3fc92d1784a5baddbc89e98091ad574b665caa
peroperje <el.pionir@gmail.com>
2020-09-18 13:18:25 +0200

    added jest emotion

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import * as emotion from "emotion";
+import { createSerializer } from "jest-emotion";
+
+// @ts-ignore
+expect.addSnapshotSerializer(createSerializer(emotion));
